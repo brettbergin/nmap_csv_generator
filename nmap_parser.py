f718c24e8613df5fbaa2a2b63041da4f77e14f6c
brett bergin <brettberginbc@yahoo.com>
2023-12-30 20:20:09 -0800

    update docstrings;pylint improvements

--- nmap_parser.py
+++ nmap_parser.py
@@ -1,19 +1,34 @@
 #!/usr/bin env python3
 
+"""
+This script is meant to help transform an nmap xml scan result file
+and create a new csv file with line items of hosts and the open ports/services
+that are discovered during the scan. The goal of this script was to learn more
+about the pandas data library and how to utilize it for data transformation 
+purposes. 
+
+Example run: ./nmap_parser.py -f some_file.xml -c foo.csv 
+alternatively: ./nmap_parser.py -f some_file.xml -j foo.json
+"""
+
 import argparse
 import datetime
-import pandas as pd
 import xml.etree.ElementTree as ET
 
+import pandas as pd
 
-def fetch_args():
-    """_summary_
+
+def fetch_args() -> argparse.ArgumentParser:
+    """
+    This function parses the command line arguments
+    provided by the user and returns the parsed arguments
+    that were found.
 
     Returns:
-        _type_: _description_
+        _type_: ArgumentParser object
     """
-    
-    parser = argparse.ArgumentParser(description="NMAP XML to CVS Parser.")
+
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(description="NMAP XML to CVS Parser.")
 
     parser.add_argument(
         "--xml_file", "-f", type=str, help="The XML input file (required)"
@@ -26,20 +41,25 @@ def fetch_args():
     return parser.parse_args()
 
 
-def parse_nmap_xml(root):
-    """_summary_
+def parse_nmap_xml(root: str) -> pd.DataFrame:
+    """
+    This function iterates over all of the host
+    objects in the XML document. For each host,
+    we continue to generate a list of all of the
+    open ports/services per network host. We return
+    a pandas Dataframe object to handle later on.
 
     Args:
-        root (_type_): _description_
+        root (_type_): string(xml file path)
 
     Returns:
-        _type_: _description_
+        _type_: Pandas DataFrame
     """
-    
-    scan_results = []
+
+    scan_results: list = []
 
     for host in root.findall("host"):
-        _h = {}
+        _h: dict = {}
 
         status = host.find("status")
         if status is not None:
@@ -64,9 +84,9 @@ def parse_nmap_xml(root):
                 _h["dns_record"] = hostname.get("name") or "None Found"
                 _h["dns_record_type"] = hostname.get("type") or "N/A"
 
-        port_list = []
+        port_list: list = []
         for port in host.findall(".//port"):
-            _p = {}
+            _p: dict = {}
             _p["protocol"] = port.get("protocol") or "N/A"
             _p["port"] = port.get("portid") or "N/A"
 
@@ -85,7 +105,7 @@ def parse_nmap_xml(root):
                 cpe = port_service.find("cpe")
                 if cpe is not None:
                     cpe_text_data = cpe.text.replace("\t", "").replace("\n", "")
-                    _p["service_cpe"] = "{}".format(cpe_text_data or "N/A")
+                    _p["service_cpe"] = f"{cpe_text_data or "N/A"}"
 
             port_list.append(_p)
 
@@ -95,66 +115,105 @@ def parse_nmap_xml(root):
     return pd.DataFrame(scan_results)
 
 
-def get_xml_tree_root(xmlfile):
+def get_xml_tree_root(xmlfile: str) -> ET.parse.getroot:
     """_summary_
 
     Args:
-        xmlfile (_type_): _description_
+        xmlfile (_type_): string of the specified xml file path.
 
     Raises:
-        FileNotFoundError: _description_
-        Exception: _description_
+        FileNotFoundError: Raises an error if the file doesnt exist.
+        Exception: General catch-all exception is something unknown goes wrong.
 
     Returns:
-        _type_: _description_
+        _type_: XML document parsed tree.
     """
-    
+
     try:
         tree = ET.parse(xmlfile)
         root = tree.getroot()
         return root
-    
+
     except FileNotFoundError as fnf_err:
-        raise FileNotFoundError(f"Unable to find file. Error: {fnf_err}")        
+        raise FileNotFoundError(f'Unable to find file. Error: {fnf_err}') from fnf_err
 
     except Exception as err:
-        raise Exception(f"Unknown Error: {err}")        
-   
+        raise Exception(f'Unknown Error Parsing XML file: {err}') from err
+
 
 def transform_data(data, run_time):
+    """
+    This function performs multiple things:
+    1. Calls the function that parses the XML and returns a pandas dataframe.
+    2. Since hosts can have many ports/services we use 'explode' and pd.Series
+       to flatten the one-to-many the dataset has within it.
+    3. Joins the parsed ports/services back into the host data.
+    4. removes the redundant 'port_list' column thats no longer required.
+
+    Args:
+        data (_type_): XML document as a string.
+        run_time (_type_): datetime.datetime.now() as a string.
+
+    Returns:
+        _type_: returns a pd.Dataframe of the transformed data.
+    """
+
     df1 = parse_nmap_xml(data)
-    df1["run_time"] = run_time.strftime("%Y-%m-%d_%H:%M:%S")
-    
+    df1["run_time"] = run_time
+
     df2 = df1.explode("port_list")
     df2 = df2["port_list"].apply(pd.Series)
 
     df3 = df1.join(df2)
     df3 = df3.drop("port_list", axis=1)
-    
+
     return df3
 
- 
-def main():
-    """_summary_
 
-    Raises:
-        Exception: _description_
+def get_current_datetime():
+    """
+    Gets the current time, and returns both
+    the current time as a datetime object as well as
+    a stringified version of the string. This function
+    should be called as so: date_obj, date_str = get_current_datetime()
+
+    Returns:
+        _type_: returns both the current time as a datetime object as well as
+                a stringified version of the string
     """
     now = datetime.datetime.now()
-    
+    return now, now.strftime("%Y-%m-%d_%H:%M:%S")
+
+
+def main():
+    """
+    This is the programs main function/entrypoint.
+
+    Returns: None
+    """
+    raw_start, start_time = get_current_datetime()
+    print(f"[+] Starting Nmap XML parser @: {start_time}.")
+
     args = fetch_args()
     if not args.xml_file:
-        print(f"[+] No provided xml file. See help. Quitting.")
+        print("[-] No provided xml file. See help. Quitting.")
         return
 
+    print(f"[+] Using XML File: {args.xml_file}.")
     xml_data = get_xml_tree_root(args.xml_file)
-    df = transform_data(xml_data, run_time=now)
+    df = transform_data(xml_data, run_time=start_time)
 
     if args.csv:
+        print(f"[+] Writing output to CSV file: {args.csv}.")
         df.to_csv(args.csv, index=False)
 
     if args.json:
+        print(f"[+] Writing output to JSON file: {args.json}.")
         df.to_json(args.json, indent=2, index=False, orient="table")
+
+    raw_stop, stop_time = get_current_datetime()
+    run_duration = raw_stop - raw_start
+    print(f"[+] Completed XML -> CSV @: {stop_time}. Duration: {run_duration}.")
 
 
 if __name__ == "__main__":
